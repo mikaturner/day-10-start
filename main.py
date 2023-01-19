ed476304c0e176949cd65deb658e674c4f18f71c
Mika Turner <69220717+mikaturner@users.noreply.github.com>
2023-01-19 02:26:12 +0000

    Added information on docstrings

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 #Functions with Outputs
 
-def format_name(f_name, l_name): 
+#def format_name(f_name, l_name): 
+#  full_name = (f_name + " " + l_name).title()
+#  return full_name
+
+#formated_string = format_name("mika", "TURNER")
+#print(formated_string)
+
+def format_name(f_name, l_name):
+  """Take a first and last name and format it to return the title case of the version of the name."""
+  if f_name == "" or l_name == "":
+    return "You didn't provide valid inputs."
   full_name = (f_name + " " + l_name).title()
   return full_name
 
-formated_string = format_name("mika", "TURNER")
-print(formated_string)
+print(format_name(input("What is your first name?"), input("What is your last name?")))
+
